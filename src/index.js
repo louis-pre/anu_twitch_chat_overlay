70c161b0f85ff578f1d71c4e6e07988595812d9c
Louis Pré <louispre94@gmail.com>
2024-06-07 18:42:02 +0000

    refactor: Extract createChatOverlay to its own function

--- src/index.js
+++ src/index.js
@@ -20,6 +20,44 @@ let enabled,
     chatContainer,
     iframe
 
+const createChatOverlay = () => {
+  const appendToParent = document.querySelector('.video-player__overlay')
+  chatContainer = createChatContainer()
+  appendTo = document.createElement('div')
+  iframe = createIframe(_ => {
+    const mouseEventsContainer = document.querySelector('.video-player__overlay')
+    makeResizable(chatContainer, mouseEventsContainer, iframe)
+    makeDraggable(chatContainer, mouseEventsContainer, chatContainer.querySelector('.header'), {
+      onDragEnd: _ => setSettings('position', styleToSettings(chatContainer.style, STYLE_ATTRS.POSITION)),
+      excludedElements: chatContainer.querySelectorAll('.settings, .settings *')
+    })
+
+    attachBaseStyle(iframe.contentDocument.body)
+    iframe.style = ''
+    removeClass(chatContainer, 'loading')
+    const html = document.querySelector('html'),
+          iframeHtml = iframe.contentDocument.querySelector('html'),
+          darkThemeClass = 'tw-root--theme-dark'
+    whenClassToggled(html, darkThemeClass, _ => {
+      if (hasClass(html, darkThemeClass))
+        addClass(iframeHtml, darkThemeClass)
+      else
+        removeClass(iframeHtml, darkThemeClass)
+    })
+  })
+  
+  chatContainer.addEventListener('mouseenter', _ => {
+    const chatList = iframe.contentDocument.body.querySelector('.chat-list--default')
+    if (chatList)
+      chatList.scrollTop = chatList.scrollHeight
+  })
+  chatContainer.addEventListener('mouseover', _ => addClass(iframe.contentDocument.body, 'hovered'))
+  chatContainer.addEventListener('mouseout', _ => removeClass(iframe.contentDocument.body, 'hovered'))
+  chatContainer.append(iframe)
+  appendToParent.append(appendTo)
+  appendTo.append(chatContainer)
+}
+
 const init = async currentStream => {
   if (inVOD())
     return
@@ -30,49 +68,11 @@ const init = async currentStream => {
 
   await getSettings()
   setupAutoClaimManager()
-  
-  const initialSetup = _ => {
-    const appendToParent = document.querySelector('.video-player__overlay')
-    chatContainer = createChatContainer()
-    appendTo = document.createElement('div')
-    iframe = createIframe(_ => {
-      const mouseEventsContainer = document.querySelector('.video-player__overlay')
-      makeResizable(chatContainer, mouseEventsContainer, iframe)
-      makeDraggable(chatContainer, mouseEventsContainer, chatContainer.querySelector('.header'), {
-        onDragEnd: _ => setSettings('position', styleToSettings(chatContainer.style, STYLE_ATTRS.POSITION)),
-        excludedElements: chatContainer.querySelectorAll('.settings, .settings *')
-      })
-
-      attachBaseStyle(iframe.contentDocument.body)
-      iframe.style = ''
-      removeClass(chatContainer, 'loading')
-      const html = document.querySelector('html'),
-            iframeHtml = iframe.contentDocument.querySelector('html'),
-            darkThemeClass = 'tw-root--theme-dark'
-      whenClassToggled(html, darkThemeClass, _ => {
-        if (hasClass(html, darkThemeClass))
-          addClass(iframeHtml, darkThemeClass)
-        else
-          removeClass(iframeHtml, darkThemeClass)
-      })
-    })
-    
-    chatContainer.addEventListener('mouseenter', _ => {
-      const chatList = iframe.contentDocument.body.querySelector('.chat-list--default')
-      if (chatList)
-        chatList.scrollTop = chatList.scrollHeight
-    })
-    chatContainer.addEventListener('mouseover', _ => addClass(iframe.contentDocument.body, 'hovered'))
-    chatContainer.addEventListener('mouseout', _ => removeClass(iframe.contentDocument.body, 'hovered'))
-    chatContainer.append(iframe)
-    appendToParent.append(appendTo)
-    appendTo.append(chatContainer)
-  }
 
   const toggle = createToggle()
   toggle.onclick = _ => {
     if (!chatContainer)
-      initialSetup()
+      createChatOverlay()
     enabled = !enabled
     if (enabled) {
       enable()
@@ -91,7 +91,7 @@ const init = async currentStream => {
   window._TCO.initializing = false
 
   if (enabled) {/* was enabled before the raid/scroll down */
-    initialSetup()
+    createChatOverlay()
   } else if (window._TCO.currentGlobalSettings.autoStart === 'true') {
     setTimeout(() => toggle.click(), 500);
   }
@@ -118,6 +118,7 @@ whenUrlChanged(async _ => {
   await getGlobalSettings()
   const oldStream = window._TCO.currentStream,
         newStream = getCurrentStream()
+  console.log("TEST STREAM", newStream, oldStream);
   if (newStream === oldStream)
     return
   cleanUp()
--- src/vod.js
+++ src/vod.js
@@ -26,6 +26,46 @@ let enabled,
     chatElement,
     initialParent
 
+const createChatOverlay = () => {
+  const appendToParent = document.querySelector('.video-player__overlay')
+  chatElement = document.querySelector('.chat-room__content, .video-chat__message-list-wrapper').parentNode
+  initialParent = chatElement.parentNode
+  addClass(chatElement, 'atco-dettached')
+  addClass(chatElement, 'chat-room')
+  chatElement.addEventListener('mouseenter', _ => {
+    const chatList = chatElement.querySelector('.chat-list--default')
+    if (chatList)
+      chatList.scrollTop = chatList.scrollHeight
+  })
+  chatElement.addEventListener('mouseover', _ => addClass(chatElement, 'hovered'))
+  chatElement.addEventListener('mouseout', _ => removeClass(chatElement, 'hovered'))
+
+  attachBaseStyle(chatElement)
+  appendTo = document.createElement('div')
+
+  chatContainer = createChatContainer()
+
+  if (!isRealVOD() && !isInChatRoom())
+    joinChatRoom()
+  
+  removeClass(chatContainer, 'loading')
+
+  attachTo(chatElement, chatContainer)
+  appendToParent.append(appendTo)
+  appendTo.append(chatContainer)
+
+  applyBackground(settingsToStyle(window._TCO.currentSettings.background, STYLE_ATTRS.BACKGROUND))
+  applyFont(settingsToStyle(window._TCO.currentSettings.font, STYLE_ATTRS.FONT))
+  applyToggles(settingsToStyle(window._TCO.currentSettings.toggles, STYLE_ATTRS.TOGGLES))
+
+  const mouseEventsContainer = document.querySelector('.video-player__overlay')
+  makeResizable(chatContainer, mouseEventsContainer)
+  makeDraggable(chatContainer, mouseEventsContainer, chatContainer.querySelector('.header'), {
+    onDragEnd: _ => setSettings('position', styleToSettings(chatContainer.style, STYLE_ATTRS.POSITION)),
+    excludedElements: chatContainer.querySelectorAll('.settings, .settings *')
+  })
+}
+
 const init = async currentVOD => {
   window._TCO.currentVOD = currentVOD
   if (!currentVOD || window._TCO.initializing)
@@ -36,51 +76,11 @@ const init = async currentVOD => {
 
   await getSettings()
   setupAutoClaimManager()
-  
-  const initialSetup = _ => {
-    const appendToParent = document.querySelector('.video-player__overlay')
-    chatElement = document.querySelector('.chat-room__content, .video-chat__message-list-wrapper').parentNode
-    initialParent = chatElement.parentNode
-    addClass(chatElement, 'atco-dettached')
-    addClass(chatElement, 'chat-room')
-    chatElement.addEventListener('mouseenter', _ => {
-      const chatList = chatElement.querySelector('.chat-list--default')
-      if (chatList)
-        chatList.scrollTop = chatList.scrollHeight
-    })
-    chatElement.addEventListener('mouseover', _ => addClass(chatElement, 'hovered'))
-    chatElement.addEventListener('mouseout', _ => removeClass(chatElement, 'hovered'))
-
-    attachBaseStyle(chatElement)
-    appendTo = document.createElement('div')
-
-    chatContainer = createChatContainer()
-
-    if (!isRealVOD() && !isInChatRoom())
-      joinChatRoom()
-    
-    removeClass(chatContainer, 'loading')
-
-    attachTo(chatElement, chatContainer)
-    appendToParent.append(appendTo)
-    appendTo.append(chatContainer)
-
-    applyBackground(settingsToStyle(window._TCO.currentSettings.background, STYLE_ATTRS.BACKGROUND))
-    applyFont(settingsToStyle(window._TCO.currentSettings.font, STYLE_ATTRS.FONT))
-    applyToggles(settingsToStyle(window._TCO.currentSettings.toggles, STYLE_ATTRS.TOGGLES))
-
-    const mouseEventsContainer = document.querySelector('.video-player__overlay')
-    makeResizable(chatContainer, mouseEventsContainer)
-    makeDraggable(chatContainer, mouseEventsContainer, chatContainer.querySelector('.header'), {
-      onDragEnd: _ => setSettings('position', styleToSettings(chatContainer.style, STYLE_ATTRS.POSITION)),
-      excludedElements: chatContainer.querySelectorAll('.settings, .settings *')
-    })
-  }
 
   const toggle = createToggle()
   toggle.onclick = _ => {
     if (!chatContainer)
-      initialSetup()
+      createChatOverlay()
     enabled = !enabled
     if (enabled) {
       enable();
@@ -100,7 +100,7 @@ const init = async currentVOD => {
   window._TCO.initializing = false
 
   if (enabled) { /* was enabled before the video switch */
-    initialSetup()
+  createChatOverlay()
   } else if (window._TCO.currentGlobalSettings.autoStart === 'true') {
     setTimeout(() => toggle.click(), 500);
   }
@@ -129,6 +129,7 @@ whenUrlChanged(async _ => {
   await getGlobalSettings()
   const oldVideo = window._TCO.currentVOD,
         newVideo = getCurrentVOD()
+  console.log("TEST VOD", oldVideo, newVideo);
   if (newVideo === oldVideo)
     return
   cleanUp()
